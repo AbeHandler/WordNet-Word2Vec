56d431cbf3ace153abe9a0025a8388816f19f3f8
Abe <abe@shorty.cs.uno.edu>
2014-09-29 23:25:35 -0500

    night

--- reverb_output_shorter.sh
+++ reverb_output_shorter.sh
@@ -1,1 +1,1 @@
-cat reverb_wikipedia_tuples.txt | awk -F '\t' '{print $4, $5, $6}'
+cat reverb_wikipedia_tuples.txt | awk -F '\t' '{print $4, $5, $6}' | tr '[:upper:]' '[:lower:]' |  tr -d 0-9 > reverb_wikipedia_tuples_short.txt
