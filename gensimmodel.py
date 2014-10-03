416e2ea355998d66adfdd9d1bfaeef51ea83742d
Abe Handler <akh2103@gmail.com>
2014-10-02 19:18:08 -0500

    workin

--- gensimmodel.py
+++ gensimmodel.py
@@ -13,13 +13,13 @@ class MySentences(object):
      def __iter__(self):
          for fname in os.listdir(self.dirname):
              for line in open(os.path.join(self.dirname, fname)):
-                 line = line.lower()
-                 yield st.stem([i for i in line.split()])
+                 yield ([st.stem(i) for i in line.decode('ascii', 'ignore').lower().split()])
 
 
-location = "/home/abe-lens-laptop/nltk_data/corpora/gutenberg"
 
-fn = location.replace("/", "")
+location = sys.argv[1]
+
+fn = location.replace("/", "") + "model"
 
 if not os.path.isfile(fn):
     sentences = MySentences(location) # a memory-friendly iterator
@@ -28,7 +28,6 @@ if not os.path.isfile(fn):
 
 model = gensim.models.Word2Vec.load(fn)
 
-print len(model.vocab)
 array = []
 numbers={}
 words={}
@@ -41,49 +40,19 @@ for w in model.vocab.keys():
 
 distances = np.zeros((counter, counter))
 
-print model.similarity("man", "woman")
-print model.similarity("man", "horse")
-
 for i in range(counter):
 	for j in range(counter):
 		distances[i,j] = model.similarity(words[i], words[j])
-		distances[j,i] = model.similarity(words[j], words[i])
 
-print distances[numbers["man"], numbers["woman"]]
+ret = [i[0] for i in np.where(distances == distances.max())]
 
-distances.dump("my_matrix.dat")
-distances = np.load("my_matrix.dat")
+print words[ret[0]]
+print words[ret[1]]
 
-'''
-import numpy as np
-from sklearn.cluster import DBSCAN
-from sklearn import metrics
-from sklearn.datasets.samples_generator import make_blobs
-from sklearn.preprocessing import StandardScaler
-from pylab import *
+#print distances
 
-Generate sample data
-centers = [[1, 1], [-1, -1], [1, -1]]
-X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4, random_state=0)
-X = StandardScaler().fit_transform(X) 
+#print np.nanargmin(distances)
+#print np.nanargmin(distances)
 
-xx, yy = zip(*X)
-scatter(xx,yy)
-
-print 'running scan'
-db = DBSCAN(eps=.05, min_samples=5).fit(myarray)
-
-
-core_samples = db.core_sample_indices_
-labels = db.labels_
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-
-clusters = [myarray[labels == i] for i in xrange(n_clusters_)]
-
-for c in clusters:
-	print "###"
-	for e in c:
-		b = e.view(np.uint8)
-		b = hashlib.sha1(b).hexdigest()
-		sys.stdout.write(numbers[b] + " ")
-'''
+distances.dump(fn + ".dat")
+distances = np.load(fn + ".dat")
