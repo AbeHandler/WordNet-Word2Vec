ea07e2da4a8495bd03a75513d18085ee16367b4b
akh2103 <akh2103@gmail.com>
2014-10-22 19:46:44 -0500

    f

--- scattermaker.sh
+++ scattermaker.sh
@@ -1,1 +1,1 @@
-python likelyhood_calculator.py --relation_file textfiles/total_"$1"_counts.txt --k_file textfiles/total_counts.txt | python scatterploy.py "$1""scatter"
+python likelyhood_calculator.py --relation_file textfiles/total_"$1"_counts.txt --k_file textfiles/total_counts.txt | python scatterploy.py images/"$1""scatter"
