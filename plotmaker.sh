6c25853fc30ad76a66dca7938425dcaf6f2ecdd0
akh2103 <akh2103@gmail.com>
2014-11-02 22:38:56 -0600

    fixes to plots

--- plotmaker.sh
+++ plotmaker.sh
@@ -1,20 +1,4 @@
 #generate the plots
-./scattermaker.sh syn
-./scattermaker.sh hypo
-./scattermaker.sh hyper
-./scattermaker.sh holo
-./scattermaker.sh mero
-./scattermaker.sh adjusted_syn
-./scattermaker.sh adjusted_hypo
-./scattermaker.sh adjusted_hyper
-./scattermaker.sh adjusted_holo
-./scattermaker.sh adjusted_mero
-
-cat textfiles/total_mero_counts.txt |  awk -F","  '{print $2}'  |  python chartmaker.py Meronym_Distance
-cat textfiles/total_holo_counts.txt |  awk -F","  '{print $2}'  |  python chartmaker.py Holonym_Distance
-cat textfiles/total_syn_counts.txt |  awk -F","  '{print $2}'  |  python chartmaker.py Synonym_Distance
-cat textfiles/total_hyper_counts.txt |  awk -F","  '{print $2}'  | python chartmaker.py Hypernym_Distance
-cat textfiles/total_hypo_counts.txt  |  awk -F","  '{print $2}'  | python chartmaker.py Hyponym_Distance
 
 python total_count_chart_maker_adjusted.py
 python total_count_chart_maker.py
